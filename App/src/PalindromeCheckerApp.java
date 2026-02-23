1461f5fb7bbe6a847f0858fc7d3b7c2a580a3ab6
adikumar1410 <ak5722@srmist.edu.in>
2026-02-23 10:53:41 +0530

    Vaishnav Added Usecase  Related Name

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,19 @@
+//usecase2;
 public class PalindromeCheckerApp {
     public static  void main(String[] args){
-        System.out.println("Welcome to Palindrome checker app");
+        String input = "madam";   // Hardcoded input
+        boolean isPalindrome = true;
+
+        // Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+
     }
 }
